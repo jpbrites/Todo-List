bf64eb12f5cfd1587109d16f42a5b4bcff3a0e56
jpbrites <74506182+JP-BRITES@users.noreply.github.com>
2024-04-11 11:30:30 -0300

    jsj

--- src/pages/delete/DeleteTask.jsx
+++ src/pages/delete/DeleteTask.jsx
@@ -1,23 +1,21 @@
 // DeleteTask.js
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom'; // Importe useParams para acessar parâmetros da URL e Link para navegação
+import { useParams, Link } from 'react-router-dom';
 import './deletetask.css';
 import Snackbar from '../../components/snackbar/Snackbar';
 
 function DeleteTask() {
-    const { id } = useParams(); // Use useParams para acessar parâmetros da URL
+    const { id } = useParams(); 
     const [task, setTask] = useState(null);
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        // Simulação de busca de dados da tarefa a ser deletada
-        fetchTaskData(id); // Use o ID da tarefa para buscar os dados
+        fetchTaskData(id);
 
-        // Função para buscar os dados da tarefa
         async function fetchTaskData(taskId) {
             try {
-                const response = await fetch(`http://127.0.0.1:8000/api/list/${taskId}`); // Substitua pela sua rota de API para buscar os detalhes da tarefa
+                const response = await fetch(`http://127.0.0.1:8000/api/list/${taskId}`); 
                 if (!response.ok) {
                     throw new Error('Erro ao buscar os dados da tarefa');
                 }
@@ -25,10 +23,9 @@ function DeleteTask() {
                 setTask(data);
             } catch (error) {
                 console.error('Erro ao buscar os dados da tarefa:', error);
-                // Lide com o erro de busca aqui
             }
         }
-    }, [id]); // Adicione id como dependência para refetching quando o ID mudar
+    }, [id]);
 
     const handleDelete = async () => {
         try {
@@ -70,7 +67,6 @@ function DeleteTask() {
                 </div>
             )}
             <Snackbar message={successMessage || errorMessage} isSuccess={successMessage !== ''} />
-
         </div>
     );
 }
